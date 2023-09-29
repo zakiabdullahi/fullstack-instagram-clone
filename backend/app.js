f72df4301ebd5cfe80621cc142c693a9e2eb78ce
Zaki abdullahi <qable2121@gmail.com>
2023-09-29 19:07:02 +0300

    added producion

--- backend/app.js
+++ backend/app.js
@@ -21,6 +21,18 @@ app.use('/api/v1/posts', postRouter)
 
 const PORT = 8000
 
+if (process.env.NODE_ENV === 'production') {
+    const __dirname = path.resolve();
+    app.use(express.static(path.join(__dirname, '/frontend/dist')));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
+    })
+
+} else {
+    app.get('/api', (req, res) => {
+        res.send('API is running....')
+    })
+}
 
 
 app.listen(PORT, () => console.log(`server  listening on port ${PORT}`))
